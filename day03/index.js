66d7742cdf4a6d0f6263baf7e4f7fd26d1227975
dazzlingtea <kitsnr08@gmail.com>
2024-02-22 13:42:50 +0900

    웹표준 백업

---
+++ day03/index.js
@@ -0,0 +1,33 @@
+1+1;
+
+100 + 200;
+
+// 아래 코드는 3000 숫자를 출력하는 코드입니다.
+console.log(3000);
+
+/* 100
+200
+300
+2*1
+2*2
+2*3
+2*4
+2*5
+2*6 */
+2*7
+
+console.log(2*1);
+console.log(2*2);
+console.log(2*3);
+console.log(2*4);
+console.log(2*5);
+console.log(2*6);
+console.log(2*7);
+console.log(2*8);
+console.log(2*9);
+
+typeof 2020;
+typeof '안녕하세요';
+typeof true;
+
+
